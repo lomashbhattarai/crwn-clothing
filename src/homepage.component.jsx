c78f0b4f263c171ef86a52bbe29416726c7a7503
lomashbhattarai <lomashbhattarai1@gmail.com>
2019-12-11 15:56:58 +0545

    first commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './homepage.styles.scss'
+const HomePage = () => (
+    <div className='homepage'>
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className='title'> HATS </h1>
+                    <span className="subtitle"> Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
+
