8cb913db471bc7404e1c3c3a7ee7024cb446a22a
lomashbhattarai <lomashbhattarai1@gmail.com>
2019-12-13 15:26:22 +0545

    header component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch , Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.jsx'
+
 const HatsPage = () => (
   <div>
     <h1> hats pddage </h1>
@@ -13,6 +15,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
